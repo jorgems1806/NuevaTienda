cd041652b923df4c0925b2b1fae74ac4b77e550e
jorgems18 <87152140+jorgems18@users.noreply.github.com>
2022-02-22 21:42:56 -0600

    Cuarto commit

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.tienda.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+@Slf4j
+
+public class IndexController
+{
+   @GetMapping("/")
+   public String inicio(Model model)
+   {
+       log.info("Estamos usando una arquitectura mvc");
+       var mensaje= "Esto es para el modelo";
+       model.addAttribute("mensaje",mensaje);
+       return "Index";
+   }
+}
